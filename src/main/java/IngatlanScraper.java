bcd01d7eb2a5b11486cf0657f8e0df66746f5ec3
Igaunian <igaunian@gmail.com>
2021-04-15 22:50:36 +0200

    add some detailed data

--- src/main/java/IngatlanScraper.java
+++ src/main/java/IngatlanScraper.java
@@ -4,16 +4,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class IngatlanScraper {
 
     final String baseUrl = "https://ingatlan.com";
     final String url = "/szukites/elado+lakas+uj-epitesu+ujszeru+felujitott+jo-allapotu+nem-berleti-jog+terulet-szerint-csokkeno+budapest+7-15-mFt+10-m2-felett+foldszint-10-felett-emelet?page=";
 
-    List<String> links = new ArrayList<>();
+    // link : list of data
+    Map<String, List<String>> scrapedData = new HashMap<>();
 
-    public void scrapeLinks() {
+    public void scrapeBasicData() {
         int page = 1;
 
         while (true) {
@@ -27,12 +30,17 @@ public class IngatlanScraper {
                 Elements elements = document.select("div.listing__card");
 
                 for (Element element : elements) {
-                    final String link = baseUrl+element.select("a.listing__link.js-listing-active-area").attr("href");
-//                    final String price = element.select("div.price").text();
-//                    final String m2 = element.select("div.listing__parameter.listing__data--area-size").text();
-//                    final String m2Price = element.select("div.price--sqm").text();
+                    List<String> data = new ArrayList<>();
 
-                    links.add(link);
+                    final String link = baseUrl+element.select("a.listing__link.js-listing-active-area").attr("href");
+                    final String price = element.select("div.price").text();
+                    final String m2 = element.select("div.listing__parameter.listing__data--area-size").text();
+                    final String m2Price = element.select("div.price--sqm").text();
+
+                    data.add(price);
+                    data.add(m2);
+                    data.add(m2Price);
+                    scrapedData.put(link, data);
                 }
 
                 page += 1;
@@ -43,20 +51,28 @@ public class IngatlanScraper {
 
         }
 
-        System.out.println(links.size());
+        System.out.println(scrapedData.size());
 
     }
 
-    public void scrapeData() {
+    public void scrapeDetailedData() {
 
-        for (String link : links) {
+        for (Map.Entry<String, List<String>> entry : scrapedData.entrySet()) {
             try {
+                String link = entry.getKey();
                 Document document = Jsoup.connect(link).get();
 
                 String header = document.select("h1.js-listing-title").text();
                 String district = header.split("\\.")[0];
-                System.out.println(district);
+                entry.getValue().add(district);
 
+                String floor = document.select("div.paramterers:nth-child(2) table:nth-child(1) tbody:nth-child(1) tr:nth-child(5) > td:nth-child(2)").text();
+                entry.getValue().add(floor);
+
+                String description = document.select("div.long-description").text();
+                entry.getValue().add(description);
+
+                System.out.println(entry.getValue());
 
             } catch (Exception ex) {
                 ex.printStackTrace();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         IngatlanScraper ingatlanScraper = new IngatlanScraper();
-        ingatlanScraper.scrapeLinks();
-        ingatlanScraper.scrapeData();
+        ingatlanScraper.scrapeBasicData();
+        ingatlanScraper.scrapeDetailedData();
     }
 }
